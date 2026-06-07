f8e7518f29201f36c97a3bbcfabda62c746d103b
Nestors <radchuk.olenaa@gmail.com>
2026-06-07 23:33:33 +0300

    add average sales comment

--- analysis.py
+++ analysis.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from numpy.ma.extras import average
+
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
 print("Продажі по містах:")
 print(df)
-print("Середнє значення:", df["sales"].mean())
+average_sales = df['sales'].mean()
+print('Середнє значення:', average_sales)
+print("Це середній рівень продажу по трьох містах")
